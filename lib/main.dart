d98a7494d8fc5edf05b40e36ccd659ba430b8afd
minsu kang <kms920612@gmail.com>
2023-10-18 13:02:02 +0900

    grammer tutorial done

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/pages/first_page.dart';
+import 'package:flutter_tutorial/pages/home_page.dart';
+import 'package:flutter_tutorial/pages/settings_page.dart';
 
 void main() {
-  runApp(MyApp());
-  print(8 / 3);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  // VARIABLES: You can store different types of info into variables
-  String name = "Dylan";
-  int age = 30;
-  double pi = 3.141592;
-  bool isBeginner = true;
-
-  /*
-
-  BASIC MATH OPERATORS:
-  1 + 1 = 2, add
-  4 - 1 = 3, subtract
-  2 * 3 = 6, multiply
-  8 / 2 = 2, divide
-  9 % 4 = 1, remainder
-
-  COMPARISON OPERATORS:
-  5 == 5 -> true, EQUAL TO
-  2 != 3 -> true, NOT EQUAL TO
-  3 > 2 -> true, GREATER THAN
-  3 < 2 -> false, LESS THAN
-  5 >= 5 -> true, GREATER OR EQUAL TO
-  3 <= 7 -> false, LESS OR EQUAL TO
-
-  LOGICAL OPERATORS:
-  AND operator, returns true if both sides are true
-  isBeginner && ( age < 18 ) -> return false
-
-  OR operator, returns true if at least one side is true
-  isBegineer || ( age < 18 ) -> return true
-
-  NOT operator, returns the opposite value
-  !isBeginner -> return false
-
-  */
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: FirstPage(),
+      routes: {
+        "/firstpage": (context) => FirstPage(),
+        "/homepage": (context) => const HomePage(),
+        "/settingspage": (context) => const SettingsPage(),
+      },
     );
   }
 }
